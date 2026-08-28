1fc4a6564dc9757c130cd484bf1d2fdf22e43fe8
dharanish-07 <dharaneesh9791@gmail.com>
2026-08-28 16:33:23 +0530

    Create contains duplicate II .java

---
+++ Arrays/contains duplicate II .java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) hs.remove(nums[i-k-1]);
+            if(!hs.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
