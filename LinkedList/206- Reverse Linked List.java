705dec60e806b2ee3a06f4b222b89f6e687f77c6
dharanish-07 <dharaneesh9791@gmail.com>
2026-09-09 12:12:34 +0530

    Add countCommas method to Solution class

---
+++ LinkedList/206- Reverse Linked List.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countCommas(long n) {
+        long count = 0;
+
+        if (n >= 1000) {
+            count += n - 1000 + 1;
+        }
+
+        if (n >= 1000000) {
+            count += n - 1000000 + 1;
+        }
+
+        if (n >= 1000000000) {
+            count += n - 1000000000 + 1;
+        }
+
+        return count;
+    }
+}
