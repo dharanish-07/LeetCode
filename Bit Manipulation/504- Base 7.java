85679cdccee2f481488596790a53fb42599121d8
dharanish-07 <dharaneesh9791@gmail.com>
2026-09-15 09:38:01 +0530

    Add method to convert integer to base 7

---
+++ Bit Manipulation/504- Base 7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num,7);
+    }
+}
+
+
+// Using math
+// class Solution {
+//     public String convertToBase7(int num) {
+//         if (num == 0) return "0";
+
+//         boolean negative = num < 0;
+//         num = Math.abs(num);
+//         StringBuilder sb = new StringBuilder();
+
+//         while (num > 0) {
+//             int rem = num % 7;
+//             sb.append(rem);   // append remainder
+//             num /= 7;         // divide by 7
+//         }
+
+//         if (negative) sb.append('-');
+
+//         return sb.reverse().toString();
+//     }
+// }
